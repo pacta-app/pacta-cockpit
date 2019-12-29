4a45709dbae901bd73b1fe13e31a92203a0d2234
Simon Buechi <simon.buechi@gmail.com>
2019-12-29 14:48:47 +0100

    add build to docs for github pages

---
+++ docs/precache-manifest.b425fb87622ccdb508be2ece87bab940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d9afda2e634e6a6bec6a17ed627e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5f91aa88bb960ec750d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b26a21f31a5ce99ecd1",
+    "url": "/static/js/2.c3f27465.chunk.js"
+  },
+  {
+    "revision": "10329eee83b5c4c57cc9af9af6dce1a0",
+    "url": "/static/js/2.c3f27465.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5f91aa88bb960ec750d",
+    "url": "/static/js/main.d3c9e6bc.chunk.js"
+  },
+  {
+    "revision": "9a048715a87806d0767a",
+    "url": "/static/js/runtime-main.3cbcab03.js"
+  }
+]);
