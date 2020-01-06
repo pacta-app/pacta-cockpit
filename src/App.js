7fcf492b683c3921a329bd5d9d013ab9a52b1089
Simon Buechi <simon.buechi@gmail.com>
2020-01-06 16:22:12 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Admin, Resource } from 'react-admin';
-import buildGraphQLProvider, { buildQueryFactory } from 'ra-data-graphql';
+import buildGraphQLProvider from 'ra-data-graphql-simple';
 //import buildGraphcoolProvider from 'ra-data-graphcool';
 //import jsonServerProvider from 'ra-data-json-server'; // demo only
 import { UserList } from './users';
 import { TestList } from './test';
 import Dashboard from './Dashboard';
-//import buildQuery from './buildQuery';
+import buildQuery from './buildQuery.json';
 // const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-
 
 class App extends Component {
   constructor() {
@@ -16,14 +15,11 @@ class App extends Component {
     this.state = { dataProvider: null };
   }
 
-  componentDidMount() {
 
-    buildGraphQLProvider({ 
-      buildQuery: [],
-      clientOptions: { uri: 'https://fakeql.com/graphql/865bb4b7a209a2a718c7ca6c94534827' },
-    })
+  componentDidMount() {
+    buildGraphQLProvider({ clientOptions: { uri: 'https://fakeql.com/graphql/48df4f7ac538c7ab84430417faf04407' }})
         .then(dataProvider => this.setState({ dataProvider }));
-  }
+}
 
 
   render() {
